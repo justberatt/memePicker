2bb4e5f75543db08213b0ad3d947a660851b2ddc
justberatt <berat.sakipi@gmail.com>
2024-09-15 02:49:18 +0200

    fixed the duplicates using the .includes() method, fixed the UI of the emotions and added the radio buttons

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion))
+                emotionsArray.push(emotion)
         }
     }
     return emotionsArray
@@ -17,7 +18,17 @@ function renderEmotionsRadios(cats){
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input 
+                type="radio"
+                id="${emotion}"
+                value="${emotion}"
+                name="emotions"
+                >
+        </div>
+        `
     }
     emotionRadios.innerHTML = radioItems
 }
